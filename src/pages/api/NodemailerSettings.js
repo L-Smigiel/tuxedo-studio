8f8920e27833f5208d3f6be6fbbfbc971345261f
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2024-04-06 21:00:59 +0200

    error handling improve

--- src/pages/api/NodemailerSettings.js
+++ src/pages/api/NodemailerSettings.js
@@ -1,46 +1,40 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
-export default async function NodemailerSettings (req, res) {
-    const { name, email, message } = req.body
-
-    const mailUser = process.env.MAIL_USER
-    const mailPass = process.env.MAIL_PASS
-    
-    const mailData = {
-        name, email, message
-    }
+export default async function NodemailerSettings(req, res) {
+    const { name, email, message } = req.body;
 
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
         secure: true,
         auth: {
-            user: mailUser,
-            pass: mailPass,
-        }
-    })
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        },
+    });
 
     try {
+        // Verify connection configuration
+        await transporter.verify();
+        console.log("Server is ready to take our messages");
 
-        const mail = await transporter.sendMail({
-            from: mailUser,
-            to: "malgorzata.drozdek@studio-tuxedo.com, lukasz.smigiel@studio-tuxedo.com", // you have to retrieve the emails from the database later
+        const mailData = {
+            from: process.env.MAIL_USER,
             replyTo: email,
+            to: "malgorzata.drozdek@studio-tuxedo.com, lukasz.smigiel@studio-tuxedo.com",
             subject: `Contact form submission from ${name}`,
-            html:`
-            <p>Name: ${name}</p>
-            <p>Email: ${email}</p>
-            <p>Message: ${message}</p>
-            `,
-        })
-        console.log("Message sent:", mail.messageId)
-        return res.status(200).json({ message: "success"})
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "Le message n'a pas pu être envoyé en raison d'une erreur, veuillez réessayer plus tard.",
-        })
-    }
+            html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+        };
 
-    return res.status(200).json({ message: "success" })
+        // Send mail
+        const info = await transporter.sendMail(mailData);
+        console.log("Message sent:", info.messageId);
+
+        return res.status(200).json({ message: "success" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Le message n'a pas pu être envoyé en raison d'une erreur, veuillez réessayer plus tard.",
+        });
+    }
 }
--- src/pages/api/dist/NodemailerSettings.dev.js
+++ src/pages/api/dist/NodemailerSettings.dev.js
@@ -10,58 +10,52 @@ var _nodemailer = _interopRequireDefault(require("nodemailer"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function NodemailerSettings(req, res) {
-  var _req$body, name, email, message, mailUser, mailPass, mailData, transporter, mail;
+  var _req$body, name, email, message, transporter, mailData, info;
 
   return regeneratorRuntime.async(function NodemailerSettings$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
           _req$body = req.body, name = _req$body.name, email = _req$body.email, message = _req$body.message;
-          mailUser = process.env.MAIL_USER;
-          mailPass = process.env.MAIL_PASS;
-          mailData = {
-            name: name,
-            email: email,
-            message: message
-          };
           transporter = _nodemailer["default"].createTransport({
             host: "smtp.gmail.com",
             port: 465,
             secure: true,
             auth: {
-              user: mailUser,
-              pass: mailPass
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS
             }
           });
-          _context.prev = 5;
-          _context.next = 8;
-          return regeneratorRuntime.awrap(transporter.sendMail({
-            from: mailUser,
-            to: "malgorzata.drozdek@studio-tuxedo.com, lukasz.smigiel@studio-tuxedo.com",
-            // you have to retrieve the emails from the database later
+          _context.prev = 2;
+          _context.next = 5;
+          return regeneratorRuntime.awrap(transporter.verify());
+
+        case 5:
+          console.log("Server is ready to take our messages");
+          mailData = {
+            from: process.env.MAIL_USER,
             replyTo: email,
+            to: "malgorzata.drozdek@studio-tuxedo.com, lukasz.smigiel@studio-tuxedo.com",
             subject: "Contact form submission from ".concat(name),
-            html: "\n            <p>Name: ".concat(name, "</p>\n            <p>Email: ").concat(email, "</p>\n            <p>Message: ").concat(message, "</p>\n            ")
-          }));
+            html: "<p>Name: ".concat(name, "</p><p>Email: ").concat(email, "</p><p>Message: ").concat(message, "</p>")
+          }; // Send mail
 
-        case 8:
-          mail = _context.sent;
-          console.log("Message sent:", mail.messageId);
+          _context.next = 9;
+          return regeneratorRuntime.awrap(transporter.sendMail(mailData));
+
+        case 9:
+          info = _context.sent;
+          console.log("Message sent:", info.messageId);
           return _context.abrupt("return", res.status(200).json({
             message: "success"
           }));
 
-        case 13:
-          _context.prev = 13;
-          _context.t0 = _context["catch"](5);
-          console.log(_context.t0);
-          res.status(500).json({
+        case 14:
+          _context.prev = 14;
+          _context.t0 = _context["catch"](2);
+          console.error(_context.t0);
+          return _context.abrupt("return", res.status(500).json({
             message: "Le message n'a pas pu être envoyé en raison d'une erreur, veuillez réessayer plus tard."
-          });
-
-        case 17:
-          return _context.abrupt("return", res.status(200).json({
-            message: "success"
           }));
 
         case 18:
@@ -69,5 +63,5 @@ function NodemailerSettings(req, res) {
           return _context.stop();
       }
     }
-  }, null, null, [[5, 13]]);
+  }, null, null, [[2, 14]]);
 }
